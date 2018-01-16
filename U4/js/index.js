6c6dc10125fdc1faa4ea0b767beabce1504e3d5a
Yu Chian <chian9512@gmail.com>
2018-01-16 17:47:04 +0800

    L3 - Hero Project - Unit 4 Sample Code

---
+++ U4/js/index.js
@@ -0,0 +1,79 @@
+class BaseCharacter {
+  constructor(name, hp, ap) {
+    this.name = name;
+    this.hp = hp;
+    this.maxHp = hp;
+    this.ap = ap;
+    this.alive = true;
+  }
+  attack(character, damage) {
+    if (this.alive == false) {
+      return;
+    }
+    character.getHurt(damage);
+  }
+  getHurt(damage) {
+    this.hp -= damage;
+    if (this.hp <= 0) {
+      this.die();
+    }
+  }
+  die() {
+    this.alive = false;
+  }
+  updateHtml(hpElement, hurtElement) {
+    hpElement.textContent = this.hp;
+    hurtElement.style.width = (100 - this.hp / this.maxHp * 100) + "%";
+  }
+}
+
+class Hero extends BaseCharacter {
+  constructor(name, hp, ap) {
+    super(name, hp, ap);
+
+    this.element = document.getElementById("hero-image-block");
+    this.hpElement = document.getElementById("hero-hp");
+    this.maxHpElement = document.getElementById("hero-max-hp");
+    this.hurtElement = document.getElementById("hero-hp-hurt");
+
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+
+    console.log("召喚英雄 " + this.name + "！");
+  }
+  attack(character) {
+    var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
+    super.attack(character, Math.floor(damage));
+  }
+  getHurt(damage) {
+    super.getHurt(damage);
+    this.updateHtml(this.hpElement, this.hurtElement);
+  }
+}
+
+class Monster extends BaseCharacter {
+  constructor(name, hp, ap) {
+    super(name, hp, ap);
+
+    this.element = document.getElementById("monster-image-block");
+    this.hpElement = document.getElementById("monster-hp");
+    this.maxHpElement = document.getElementById("monster-max-hp");
+    this.hurtElement = document.getElementById("monster-hp-hurt");
+
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+
+    console.log("遇到怪獸 " + this.name + "了！");
+  }
+  attack(character) {
+    var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
+    super.attack(character, Math.floor(damage));
+  }
+  getHurt(damage) {
+    super.getHurt(damage);
+    this.updateHtml(this.hpElement, this.hurtElement);
+  }
+}
+
+var hero = new Hero("Bernard", 130, 30);
+var monster = new Monster("Skeleton", 130, 10);
